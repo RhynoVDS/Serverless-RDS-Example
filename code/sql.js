fcf3d9b378f76332cb103d3b36c7f895ba15a171
vandersluijsr <rhyno.vandersluijs@tabcorp.com.au>
2019-07-10 16:00:52 +1000

    Cleaning up code

--- code/sql.js
+++ code/sql.js
@@ -1,41 +1,51 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var con = mysql.createConnection({
     host:  process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD, 
     database: process.env.DATABASE
 });
-
-exports.handler = (event, context) => {
-
-    var success = false;
-
-    try
-    {
-        connection.query('show tables', function (error, results, fields) {
-            if (error) {
-                connection.destroy();
-            } else {
-                // connected!
-                success=true;
-                console.log(results);
-                connection.end(function (err) { callback(err, results);});
-            }
-        });
-    }
-    finally{}
-
-    if(success)
-    {
-        return {
-            'statusCode': 200,
-            'body': 'success!'
-        };
-    }
-    else{
-        return {
-            'statusCode': 500,
-            'body': 'bad things!'
-        };
-    }
+exports.setup = (event, context, callback) => {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+        var response = {
+            statusCode: 200,
+            body: JSON.stringify(result)
+        }
+        callback(err,response)
+    });
 };
+
+exports.query = (event, context, callbak) => {
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        var response = {
+            statusCode: 200,
+            body: JSON.stringify(result)
+        }
+        callback(err,json.stringify(response))
+    });
+}
+
+exports.insert = (event, context, callbak) => {
+    con.connect(function(err) {
+        if (err) throw err;
+        
+        var sql = "INSERT INTO customers (name, address) VALUES ('" + event.body.name + "','" + event.body.address+ "')"
+        con.query(sql, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            var response = {
+                statusCode: 200,
+                body: JSON.stringify(result)
+            }
+            callbak(err,response);
+        });
+    });
+}
