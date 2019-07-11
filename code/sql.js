f3be35b3e6373fb8f810e96ed4cd21b6cb7d1df4
vandersluijsr <rhyno.vandersluijs@tabcorp.com.au>
2019-07-11 13:52:38 +1000

    Code cleanup and bug fixes

--- code/sql.js
+++ code/sql.js
@@ -6,36 +6,40 @@ var con = mysql.createConnection({
     database: process.env.DATABASE
 });
 exports.setup = (event, context, callback) => {
-    if (err) throw err;
+    
     console.log("Connected!");
     var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
     con.query(sql, function (err, result) {
+
         if (err) throw err;
-        console.log("Table created");
+
         var response = {
             statusCode: 200,
             body: JSON.stringify(result)
         }
+
+        con.end();
         callback(err,response)
     });
 };
 
-exports.query = (event, context, callbak) => {
-    if (err) throw err;
+exports.query = (event, context, callback) => {
+
     con.query("SELECT * FROM customers", function (err, result, fields) {
-        if (err) throw err;
+
         console.log(result);
         var response = {
             statusCode: 200,
             body: JSON.stringify(result)
         }
-        callback(err,json.stringify(response))
+
+        con.end();
+        callback(err,JSON.stringify(response))
     });
 }
 
-exports.insert = (event, context, callbak) => {
+exports.insert = (event, context, callback) => {
     con.connect(function(err) {
-        if (err) throw err;
         
         var sql = "INSERT INTO customers (name, address) VALUES ('" + event.body.name + "','" + event.body.address+ "')"
         con.query(sql, function (err, result, fields) {
@@ -45,7 +49,8 @@ exports.insert = (event, context, callbak) => {
                 statusCode: 200,
                 body: JSON.stringify(result)
             }
-            callbak(err,response);
+            con.end();
+            callback(err,response);
         });
     });
 }
